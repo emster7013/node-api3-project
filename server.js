ff5a7eadb7c979ed9f5a4d98d00da88b7fd2e6fa
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-03-19 20:54:53 -0400

    set up indext and some of server

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
+server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.get('/', (req, res) =>{
+    res.send('<h1>Yo waddup</h1>');
 });
+server.use(logger);
+server.use('/users', userRouter);
 
-//custom middleware
-
-function logger(req, res, next) {}
-
+function logger(req,res,next){
+    
+}
 module.exports = server;
